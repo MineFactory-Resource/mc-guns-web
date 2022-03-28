b131402de19dd280fd66724824af2e8ec8480d40
CeylonPark <ceylonp20@gmail.com>
2022-03-29 00:59:34 +0900

    feat: Footer 분리

---
+++ pages/footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const styles = {
+
+}
+
+const Footer =() => {
+	return (
+		<div style={{width: "100%", height: "220px", backgroundColor: "#343A40"}}>
+			<div style={{width: "1280px", height: "100%", margin: "auto", display: "flex"}}>
+				
+			</div>
+		</div>
+	)
+}
+
+export default Footer
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,16 +1,7 @@
 import React from 'react'
-import Header from './header'
 import styles from '../styles/Home.module.css'
-
-const Footer =() => {
-	return (
-		<div style={{width: "100%", height: "220px", backgroundColor: "#343A40"}}>
-			<div style={{width: "1280px", height: "100%", margin: "auto", display: "flex"}}>
-				
-			</div>
-		</div>
-	)
-}
+import Header from './header'
+import Footer from './footer'
 
 const News = () => {
 	return (
