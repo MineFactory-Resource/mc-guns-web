78bed4d8260da8fd71eee9003d6d54dcf17b8368
CeylonPark <ceylonp20@gmail.com>
2022-04-06 12:44:43 +0900

    feat: 헤더 수정

--- components/header.tsx
+++ components/header.tsx
@@ -1,56 +1,18 @@
 import React from 'react'
-
-const styles = {
-    container: {
-        width: "100%",
-        height: "88px",
-        backgroundColor: "#000000"
-    },
-
-    inner_container: {
-        width: "1400px",
-        height: "100%",
-        margin: "auto",
-        display: "flex"
-    },
-
-    logo: {
-        position: "absolute",
-        top: "32px",
-        width: "57px",
-        height: "24px",
-        margin: "auto 0"
-    },
-
-    navigation: {
-        margin: "auto",
-        display: "flex",
-    },
-
-    navigation_item: {
-        margin: "0 30px 0 30px",
-        fontFamily: "Lato",
-        fontWeight: "700",
-        fontSize: "20px",
-        lineHeight: "24px",
-        display: "block",
-        aligntems: "center",
-        color: "#CFCFCF"
-    }
-}
+import styles from '../styles/Header.module.scss'
 
 const Header = () => {
     return (
-		<div style={styles.container}>
-			<div style={styles.inner_container}>
-				<img style={styles.logo}/>
-                <div style={styles.navigation}>
-                    <span style={styles.navigation_item}>HOME</span>
-                    <span style={styles.navigation_item}>RULES</span>
-                    <span style={styles.navigation_item}>FAQ</span>
+		<nav className={styles.navbar}>
+			<div className={styles.navbar__container}>
+				<img className={styles.navbar__logo}/>
+                <div className={styles.navbar__content}>
+                    <span className={styles.navbar__item}>HOME</span>
+                    <span className={styles.navbar__item}>RULES</span>
+                    <span className={styles.navbar__item}>FAQ</span>
                 </div>
 			</div>
-		</div>
+		</nav>
 	)
 }
 
